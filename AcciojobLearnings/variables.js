37a92eb15ce8164aa8dc4084c539dd7f8535ef11
karishmaMohammed <karishmamohammed43@gmail.com>
2024-10-16 18:41:30 +0530

    concepsts of class, call, apply bind

--- AcciojobLearnings/variables.js
+++ AcciojobLearnings/variables.js
@@ -55,3 +55,72 @@ let obj2 = {
 let obj3 = { ... obj2} //shallow copy this copies only one level cant go inner levels
 delete obj2.x;
 console.log(obj3.x.y.k)
+
+//deep copy revise this topic
+
+// call, apply and bind
+// oops in javascript
+// this key word is a object refer to current scope (it remember who called it)
+
+// console.log(this) => o/p {}, prints global scope which is window object
+
+// function abc(){ // writing a function in global space which does't have any parent
+//     console.log(this)
+// }
+// abc() // prints global scope which is window object 
+
+// function xyz(){
+//     function abc(){
+//         console.log(this)
+//     }
+//     abc()
+// }        o/p => // prints global scope which is window object 
+// xyz()
+
+// note = > it does't matter where it is wriiten it matters from it is called
+
+// write a function call inside object
+
+const object = {
+    name:"ABC",
+    printName:function(){
+        console.log(this) 
+    }
+}
+object.printName()
+
+// revise concept of call, apply and bind
+
+// how "this" works in arrow function
+// apply(reference, [no. of argument/parameters])
+
+// class Human{
+//     constructor(){
+//         console.log(this) // this keyword by default it returns
+
+//         return 2 // when u retun primitive variable it returns empty object bcs new keyword assign empty object
+//         // whereas returning non primitive variable like array/object it return exact those 
+//         // this is how "this" works
+//     }
+// }
+
+// const person = new Human() // new keyword creates empty object
+
+// console.log(person)
+
+class Human{
+    constructor(eyeColor, skinColor, gender, lang){
+        this.hand =2
+        this.eye = eyeColor
+        this.color = skinColor
+        this.gender = gender
+        this.speak = function(proficiency){
+           console.log(proficiency + " " + lang) 
+        }
+    }
+}
+
+const person1 = new Human("black","cream","F","english")
+const person2 = new Human("browm","black","M","Hindi")
+console.log(person1);
+console.log(person2.speak("fluent"));
